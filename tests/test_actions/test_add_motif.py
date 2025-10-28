added tests to replace.py. Fixed float precision issues caused by npt.assert_array_equal.

tests/test_actions/test_add_motif.py
@@ -92,7 +92,7 @@ def test_at_position_mode(allowed_operated_motif, orig_atoms):
         np.array([0.5, 0.5, 0.5]) @ orig_atoms.cell.complete()
     )
     # Translated, rather than rotated.
-    npt.assert_array_equal(
+    npt.assert_allclose(
         added_atoms.get_positions() - added_centroid,
         allowed_operated_motif.cart_coords - allowed_operated_motif.get_centroid(fractional=False)
     )
@@ -126,7 +126,7 @@ def test_relative_to_motif_mode(allowed_operated_motif, allowed_relative_to_moti
         added_centroid,
         expected_centroid
     )
-    npt.assert_array_equal(
+    npt.assert_allclose(
         added_atoms.get_positions() - added_centroid,
         allowed_operated_motif.cart_coords - allowed_operated_motif.get_centroid(fractional=False)
     )
@@ -161,7 +161,7 @@ def test_relative_to_position(allowed_operated_motif, orig_atoms):
         added_centroid,
         expected_centroid
     )
-    npt.assert_array_equal(
+    npt.assert_allclose(
         added_atoms.get_positions() - added_centroid,
         allowed_operated_motif.cart_coords - allowed_operated_motif.get_centroid(fractional=False)
     )
@@ -197,7 +197,7 @@ def test_relative_to_pair_motif(allowed_operated_motif, bond_motif, orig_atoms):
         added_centroid,
         expected_centroid
     )
-    npt.assert_array_equal(
+    npt.assert_allclose(
         added_atoms.get_positions() - added_centroid,
         allowed_operated_motif.cart_coords - allowed_operated_motif.get_centroid(fractional=False)
     )

tests/test_actions/test_remove_motif.py
@@ -3,16 +3,10 @@
 import pytest
 import numpy as np
 import numpy.testing as npt
-from ase import Atoms
 
 from AtomWorldBench.atom_world.actions.motif_actions.base import BaseMotifAction
 from AtomWorldBench.atom_world.actions.motif_actions.remove import RemoveMotifAction
-from AtomWorldBench.atom_world.motifs.base import BaseMotif
-from AtomWorldBench.atom_world.motifs.regions.base import BaseRegionMotif
 from AtomWorldBench.common.registry import get_registered
-
-from AtomWorldBench.atom_world.motifs.site_collections.cluster import ClusterMotif
-from AtomWorldBench.atom_world.motifs.site_collections.site import SiteMotif
 
 from ..utils import get_random_motif
 
@@ -38,7 +32,7 @@ def forbidden_remove_motif(request, orig_atoms):
 
 
 def test_registry():
-    """Test that AddMotifAction is registered correctly."""
+    """Test that RemoveMotifAction is registered correctly."""
     action_class = get_registered(BaseMotifAction)["remove-motif"]
     assert action_class is RemoveMotifAction
     action_class = get_registered(BaseMotifAction)["remove"]
@@ -61,7 +55,7 @@ def test_remove_motif_action(allowed_remove_motif):
         np.arange(len(action.operated_atoms)),
         allowed_remove_motif.indices
     ))
-    npt.assert_array_equal(
+    npt.assert_allclose(
         new_atoms.get_positions(wrap=False),
         action.operated_atoms.get_positions(wrap=False)[remaining_indices]
     )