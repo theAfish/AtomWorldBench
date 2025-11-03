1922a7130026a5b6583132f83913596db6b5ee2d
qchempku2017 <yxyhncd@gmail.com>
2025-11-03 17:44:33 +0800

    Remove src. It is not necessary in the most standard package structure.

--- src/benchmark/__init__.py
+++
@@ -1,5 +0,0 @@
-from .config import BenchmarkConfig
-from .parser import BenchmarkArgumentParser
-from .runner import BenchmarkRunner
-
-__all__ = ['BenchmarkConfig', 'BenchmarkArgumentParser', 'BenchmarkRunner']
