8d0da7694df9102a067649384bd0437504debea8
alex chen <4lexchen5@gmail.com>
2025-11-17 11:32:20 +0800

    fix: update vllm model code, push analysis results

--- src/models/vllm_model.py
+++ src/models/vllm_model.py
@@ -1,20 +1,8 @@
 import os 
-import ray
-from packaging.version import Version
-from ray.data.llm import build_llm_processor, vLLMEngineProcessorConfig
+from vllm import LLM, SamplingParams
 
 from typing import Any, Dict, List, Optional, Union
 from .base_model import BaseModel
-
-assert Version(ray.__version__) >= Version("2.44.1"), (
-    "Ray version must be at least 2.44.1"
-)
-
-# Uncomment to reduce clutter in stdout
-# ray.init(log_to_driver=False)
-# ray.data.DataContext.get_current().enable_progress_bars = False
-
-ray.data.DataContext.get_current().wait_for_min_actors_s = 60*60
 
 class vllmModel(BaseModel):
     def __init__(
@@ -37,46 +25,56 @@ class vllmModel(BaseModel):
         }
         self.default_generation_params.update(kwargs)
         
-        self.vllm_config = vLLMEngineProcessorConfig(
-            model_source=model_name,
-            engine_kwargs={
-                "trust_remote_code": True,
-                "enable_chunked_prefill": True,
-                "max_model_len": 32768,
-                "tensor_parallel_size": self.gpus_visible,
-                "pipeline_parallel_size": self.world_size,
-                "max_num_batched_tokens": 16384,
-                # "distributed_executor_backend": "ray"
-                "hf_token": os.environ.get("HF_TOKEN")
-            },
-            concurrency=1,  # set the number of parallel vLLM replicas
-            batch_size=64,
+        self.sampling_params = SamplingParams(
+            temperature=self.default_generation_params["temperature"],
+            max_tokens=16384
         )
+        self.llm = LLM(model=model_name, tensor_parallel_size=self.gpus_visible)
+
+        # self.vllm_config = vLLMEngineProcessorConfig(
+        #     model_source=model_name,
+        #     engine_kwargs={
+        #         "trust_remote_code": True,
+        #         "enable_chunked_prefill": True,
+        #         "max_model_len": 32768,
+        #         "tensor_parallel_size": self.gpus_visible,
+        #         "pipeline_parallel_size": self.world_size,
+        #         "max_num_batched_tokens": 16384,
+        #         # "distributed_executor_backend": "ray"
+        #         "hf_token": os.environ.get("HF_TOKEN")
+        #     },
+        #     concurrency=1,  # set the number of parallel vLLM replicas
+        #     batch_size=64,
+        # )
         
-        self.vllm_processor = build_llm_processor(
-            self.vllm_config,
-            preprocess=lambda row: dict(
-                messages=[
-                    {"role": "user", "content": row["item"]}
-                ],
-                sampling_params=dict(self.default_generation_params)
-            ),
-            postprocess=lambda row: dict(
-                answer=row["generated_text"],
-                **row  # This will return all the original columns in the dataset.
-            ),
-        )
+        # self.vllm_processor = build_llm_processor(
+        #     self.vllm_config,
+        #     preprocess=lambda row: dict(
+        #         messages=[
+        #             {"role": "user", "content": row["item"]}
+        #         ],
+        #         sampling_params=dict(self.default_generation_params)
+        #     ),
+        #     postprocess=lambda row: dict(
+        #         answer=row["generated_text"],
+        #         **row  # This will return all the original columns in the dataset.
+        #     ),
+        # )
         
     def generate(self, prompt: str, **kwargs) -> str:
-        return self.generate_batch([prompt], **kwargs)
+        return self.generate_batch([prompt], **kwargs)[0]
     
     def generate_batch(self, prompts: List[str], **kwargs) -> List[str]:
-        indexed_prompts = [{"idx": i, "item": p} for i, p in enumerate(prompts)]
-        ds = ray.data.from_items(indexed_prompts)
-        ds = self.vllm_processor(ds)
-        # ds = ds.materialize()
+        conversations = [[{ "role": "user", "content": prompt }] for prompt in prompts]
+        outputs = self.llm.chat(conversations, self.sampling_params)
+        return [output.outputs[0].text for output in outputs]
+
+        # indexed_prompts = [{"idx": i, "item": p} for i, p in enumerate(prompts)]
+        # ds = ray.data.from_items(indexed_prompts)
+        # ds = self.vllm_processor(ds)
+        # # ds = ds.materialize()
         
-        outputs = ds.take_all()
-        outputs.sort(key=lambda x: x["idx"])
+        # outputs = ds.take_all()
+        # outputs.sort(key=lambda x: x["idx"])
         
-        return [output["answer"] for output in outputs]
+        # return [output["answer"] for output in outputs]
